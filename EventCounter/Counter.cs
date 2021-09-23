84717848dd4f8f31f5f1410a3a25016444202aec
hxchua00 <hxchua00@gmail.com>
2021-09-23 14:37:20 +0800

    Add project files.

---
+++ EventCounter/Counter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EventCounter
+{
+    public delegate void CountEventHandler(object sender, EventArgs e);
+    class Counter
+    {
+        public event CountEventHandler CountEvent;
+        public int count = 0;
+        
+        public void IncreaseCount()
+        {
+            while (count < 21)
+            {
+                Console.WriteLine("Waiting...");
+                Thread.Sleep(5000);
+                count++;
+                Console.WriteLine("Count: " + count);
+                Console.WriteLine();
+
+                if (count == 20)
+                {
+                    raiseCountEvent(EventArgs.Empty);
+                    count = 0;
+                }
+            }
+            
+        }
+
+        public virtual void raiseCountEvent(EventArgs e)
+        {
+            CountEventHandler cEvent = CountEvent;
+            cEvent?.Invoke(this,e);
+        }
+    }
+}
---
+++ EventCounter/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EventCounter
+{
+    
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Counter c = new Counter();
+
+            c.CountEvent += c_CountEvent;
+            c.IncreaseCount();
+
+            Console.ReadLine();
+        }
+
+        public static void c_CountEvent(object sender, EventArgs e)
+        {
+            Console.WriteLine("Event Received!");
+            Console.WriteLine();
+        }
+    }
+}
